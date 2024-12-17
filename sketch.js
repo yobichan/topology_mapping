de9082fe69e4533f97f1d9a7e6f3ef8102a694e0
makosbab <robesz86@gmail.com>
2024-12-17 19:45:00 +0100

    mapping fcuntion with grayscale

--- sketch.js
+++ sketch.js
@@ -1,34 +1,38 @@
-let map;
+let topographyMap;
 
 const cellSideLength = 10;
-function preload(){
-    map = loadTable("topography_map_100x100.csv");
-}
-function setup(){
-    
 
-    createCanvas(cellSideLength * map.getRowCount(), cellSideLength * map.getColumnCount());
-    background(220);
-    console.log(width);
-    console.log(height);
-
-
-    // for (let r = 0; r < map.getRowCount(); r++)
-    // for (let c = 0; c < map.getColumnCount(); c++) {
-    //   print(map.getString(r, c));
-    // }
+function mapAltitudeToColor(altitude) {
+  //map(value, start1, stop1, start2, stop2, [withinBounds])
+  return map(altitude, 0, 5000, 0, 255);
 }
 
-function draw(){
-    for (let row = 0; row < map.getRowCount(); row++) {
-        for (let column = 0; column < map.getColumnCount(); column++) {
-            //upper left corner  x,y, height, width
-            rect(row * cellSideLength, column * cellSideLength, cellSideLength, cellSideLength)
-            fill(240)
-            
-        }
-        
-        
+function preload() {
+  topographyMap = loadTable("topography_map_100x100.csv");
+}
+
+function setup() {
+  createCanvas(
+    cellSideLength * topographyMap.getRowCount(),
+    cellSideLength * topographyMap.getColumnCount()
+  );
+  background(220);
+}
+
+function draw() {
+  for (let row = 0; row < topographyMap.getRowCount(); row++) {
+    for (let column = 0; column < topographyMap.getColumnCount(); column++) {
+      let altitude = parseInt(topographyMap.getString(row, column)); // Get altitude as an integer
+
+      let colorValue = mapAltitudeToColor(altitude);
+      //upper left corner  x,y, height, width
+      fill(colorValue);
+      rect(
+        column * cellSideLength,
+        row * cellSideLength,
+        cellSideLength,
+        cellSideLength
+      );
     }
-
+  }
 }
